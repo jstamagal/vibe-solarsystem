7473c682472354adcb1ff0ac229bb8e7ca3dedc9
jstamagal <jstamagal@gmail.com>
2026-01-18 00:09:04 -0500

    womp womp womp at me

--- the_fuck_machine.sh
+++ the_fuck_machine.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # because we fucking know where our bash is
-while test -f CIRCUIT_BREAKER.txt ; do claude -p --dangerously-skip-permissions ; done
+while test -f CIRCUIT_BREAKER.txt ; do cat PROMPT.md | claude --print --dangerously-skip-permissions ; done
 mpv "./womp womp womp.mp3"
